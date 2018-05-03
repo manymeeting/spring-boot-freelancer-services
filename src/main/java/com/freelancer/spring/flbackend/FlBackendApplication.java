7e33dfe01295ad7db96bf300a5969c2a5299e8e8
Mokuten OH <mokuteno@gmail.com>
2018-05-02 23:16:53 -0700

    init with spring boot starter

---
+++ src/main/java/com/freelancer/spring/flbackend/FlBackendApplication.java
@@ -0,0 +1,12 @@
+package com.freelancer.spring.flbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlBackendApplication.class, args);
+	}
+}
