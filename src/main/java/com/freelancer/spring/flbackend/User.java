4406b3bbbe5f4a0dccf6056d47a37de287ea81fa
Mokuten OH <mokuteno@gmail.com>
2018-05-05 15:39:31 -0700

    create user entity

---
+++ src/main/java/com/freelancer/spring/flbackend/User.java
@@ -0,0 +1,103 @@
+package com.freelancer.spring.flbackend;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Table(name="users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="user_id")
+    private Integer userId;
+
+    @NotNull
+    @Column(name="user_name")
+    private String userName;
+
+    @NotNull
+    @Column(name="user_email")
+    private String userEmail;
+
+    @NotNull
+    @Column(name="user_password")
+    private String userPassword;
+
+    @Column(name="user_avatarurl")
+    private String userAvatarUrl;
+
+    @Column(name="user_phone")
+    private String userPhone;
+
+    @Column(name="user_about")
+    private String userAbout;
+
+    @Column(name="user_skills")
+    private String userSkills;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserAvatarUrl() {
+        return userAvatarUrl;
+    }
+
+    public void setUserAvatarUrl(String userAvatarUrl) {
+        this.userAvatarUrl = userAvatarUrl;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserAbout() {
+        return userAbout;
+    }
+
+    public void setUserAbout(String userAbout) {
+        this.userAbout = userAbout;
+    }
+
+    public String getUserSkills() {
+        return userSkills;
+    }
+
+    public void setUserSkills(String userSkills) {
+        this.userSkills = userSkills;
+    }
+}
