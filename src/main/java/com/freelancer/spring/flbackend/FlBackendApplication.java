a0bb1e0d47cde1279479699a01b89217bf934122
Mokuten OH <mokuteno@gmail.com>
2018-05-07 00:19:49 -0700

    add packages for password encryption/decryption

--- src/main/java/com/freelancer/spring/flbackend/FlBackendApplication.java
+++ src/main/java/com/freelancer/spring/flbackend/FlBackendApplication.java
@@ -2,11 +2,19 @@ package com.freelancer.spring.flbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class FlBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlBackendApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
