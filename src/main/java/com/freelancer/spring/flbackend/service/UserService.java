8d7179bf07cd453b41f35b729922746e39a7f891
Mokuten OH <mokuteno@gmail.com>
2018-05-07 00:21:18 -0700

    add api for create a user

--- src/main/java/com/freelancer/spring/flbackend/service/UserService.java
+++ src/main/java/com/freelancer/spring/flbackend/service/UserService.java
@@ -1,11 +1,17 @@
 package com.freelancer.spring.flbackend.service;
 
+import com.freelancer.spring.flbackend.User;
 import com.freelancer.spring.flbackend.dto.UserDto;
 import com.freelancer.spring.flbackend.dto.UserProfileDto;
+import com.freelancer.spring.flbackend.dto.param.CreateUserDto;
 
 public interface UserService {
 
     UserDto getUserById(Integer id);
 
+    UserDto getUserByEmail(String email);
+
     UserProfileDto getUserProfileById(Integer id);
+
+    UserDto createUser(CreateUserDto createUserDto);
 }
--- src/main/java/com/freelancer/spring/flbackend/service/impl/UserServiceImpl.java
+++ src/main/java/com/freelancer/spring/flbackend/service/impl/UserServiceImpl.java
@@ -5,12 +5,17 @@ import com.freelancer.spring.flbackend.User;
 import com.freelancer.spring.flbackend.dao.UserDao;
 import com.freelancer.spring.flbackend.dto.UserDto;
 import com.freelancer.spring.flbackend.dto.UserProfileDto;
+import com.freelancer.spring.flbackend.dto.param.CreateUserDto;
 import com.freelancer.spring.flbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private UserDao userDao;
@@ -22,9 +27,24 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
+    public UserDto getUserByEmail(String email) {
+        User user = userDao.findUserByEmail(email);
+        return UserDto.mapToUserDto(user);
+    }
+
+    @Override
     public UserProfileDto getUserProfileById(Integer id) {
         User user = userDao.findUserById(id);
         return UserProfileDto.mapToUserProfileDto(user);
+    }
+
+    @Override
+    public UserDto createUser(CreateUserDto createUserDto) {
+        createUserDto.setUserPassword(passwordEncoder.encode(createUserDto.getUserPassword()));
+        User user = CreateUserDto.toUser(createUserDto);
+        user = userDao.save(user);
+
+        return UserDto.mapToUserDto(user);
     }
 
 
