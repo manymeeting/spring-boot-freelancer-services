41858e183d0503b0cde81117eefbd2ecf6ee8269
Mokuten OH <mokuteno@gmail.com>
2018-05-07 00:20:03 -0700

    add generic controller method to handle bad request

--- src/main/java/com/freelancer/spring/flbackend/controller/GenericController.java
+++ src/main/java/com/freelancer/spring/flbackend/controller/GenericController.java
@@ -19,4 +19,18 @@ public class GenericController {
         return new ResponseEntity(notFoundRes, HttpStatus.NOT_FOUND);
     }
 
+    public ResponseEntity badRequest()
+    {
+        HashMap<String, Object> badReqRes = new HashMap<>();
+        badReqRes.put("error", "Bad Request");
+        return new ResponseEntity(badReqRes, HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity badRequestWithtMsg(String message)
+    {
+        HashMap<String, Object> badReqRes = new HashMap<>();
+        badReqRes.put("error", message);
+        return new ResponseEntity(badReqRes, HttpStatus.BAD_REQUEST);
+    }
+
 }
