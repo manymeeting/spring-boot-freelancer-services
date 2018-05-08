87d6f614cbb2911b410b0d820d9913363e5bced4
Mokuten OH <mokuteno@gmail.com>
2018-05-07 23:20:48 -0700

    add project dao

---
+++ src/main/java/com/freelancer/spring/flbackend/dao/ProjectDao.java
@@ -0,0 +1,15 @@
+package com.freelancer.spring.flbackend.dao;
+
+import com.freelancer.spring.flbackend.entity.Project;
+import com.freelancer.spring.flbackend.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProjectDao extends PagingAndSortingRepository<Project, Integer> {
+
+    @Query("select p from Project p join p.employer e where e.userId = :userId")
+    List<Project> getAllProjPublishedByUser(@Param("userId") Integer userId);
+}
