cc7e70e3fe5063bbc51bc8deba502fcc9bbdc234
Mokuten OH <mokuteno@gmail.com>
2018-05-08 23:16:20 -0700

    add more test cases

--- src/test/java/com/freelancer/spring/flbackend/ApiTests.java
+++ src/test/java/com/freelancer/spring/flbackend/ApiTests.java
@@ -3,6 +3,7 @@ package com.freelancer.spring.flbackend;
 import com.freelancer.spring.flbackend.dao.BidDao;
 import com.freelancer.spring.flbackend.dao.ProjectDao;
 import com.freelancer.spring.flbackend.dao.UserDao;
+import com.freelancer.spring.flbackend.dto.ProjectDto;
 import com.freelancer.spring.flbackend.dto.UserDto;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,6 +14,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = FlBackendApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -35,6 +38,39 @@ public class ApiTests {
                 restTemplate.getForEntity("/users/111", UserDto.class);
         Assert.assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
 
+    }
+
+    @Test
+    public void shouldNotFoundProject() throws  Exception {
+
+        ResponseEntity response =
+                restTemplate.getForEntity("/projects/111", ProjectDto.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void shouldNotFoundUserProfile() throws  Exception {
+
+        ResponseEntity response =
+                restTemplate.getForEntity("/users/111/profile", UserDto.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void shouldNotFoundProjectsWithInvalidStatus() throws  Exception {
+
+        ResponseEntity response =
+                restTemplate.getForEntity("/projects/status/invalidxxx", List.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void shouldNotFoundProjectsPublishedByInvalidUser() throws  Exception {
+
+        ResponseEntity response =
+                restTemplate.getForEntity("/projects/publisher/111", List.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        Assert.assertEquals(response.getBody().toString(), "[]");
     }
 
 }
