5d59b84abfefc3bb128d87aa23a4de260ff6fd7d
Mokuten OH <mokuteno@gmail.com>
2018-05-08 20:15:11 -0700

    add api for get all bidded projects

--- src/main/java/com/freelancer/spring/flbackend/dao/ProjectDao.java
+++ src/main/java/com/freelancer/spring/flbackend/dao/ProjectDao.java
@@ -12,4 +12,10 @@ public interface ProjectDao extends PagingAndSortingRepository<Project, Integer>
 
     @Query("select p from Project p join p.employer e where e.userId = :userId")
     List<Project> getAllProjPublishedByUser(@Param("userId") Integer userId);
+
+    @Query("select p from Project p join p.bids b where b.bidder.userId = :userId")
+    List<Project> getAllProjBiddedByUser(@Param("userId") Integer userId);
+
+    @Query("select p from Project p where p.status = :status")
+    List<Project> getAllProjOnStatus(@Param("status") String status);
 }
